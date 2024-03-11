e00a6c52d219b833598616e47ae825329ff52ed0
IvanParejaLopez <ivanparejalopez@uma.es>
2024-03-11 10:49:19 +0100

    first commit

---
+++ src/main/java/es/uma/informatica/pa1/App.java
@@ -0,0 +1,13 @@
+package es.uma.informatica.pa1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
